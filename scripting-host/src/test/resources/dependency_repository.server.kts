6f8a88462897975fe523a1da026e2b91f6ad7880
EdwarDDay <4127904+EdwarDDay@users.noreply.github.com>
2024-12-29 11:19:05 +0100

    Run tests with service launch
    - fix repositories test

--- scripting-host/src/test/resources/dependency_repository.server.kts
+++ scripting-host/src/test/resources/dependency_repository.server.kts
@@ -1,9 +1,8 @@
-@file:Repository("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-@file:DependsOn("com.squareup.okhttp3:okhttp:5.0.0-SNAPSHOT")
+@file:Repository("https://dl.google.com/dl/android/maven2/")
+@file:DependsOn("androidx.collection:collection:1.4.5")
 
-import okhttp3.MediaType.Companion.toMediaType
+import androidx.collection.intListOf
 
+val list = intListOf(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99)
 
-setHeader("Content-Type", "application/json; charset=utf-8".toMediaType().toString())
-
-writeOutput("""{"status":"OK"}""")
+writeOutput(list.toString())
